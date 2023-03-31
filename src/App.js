22ff1599f15ae0e8364cbad46d815d84c675ee39
Rushi <rushi@techtic.agency>
2023-03-31 18:04:11 +0530

    first practical

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Header from './Component/header';
+import Form from './Component/Form';
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <Form />
+    </div>
+  );
+}
+
+export default App;
