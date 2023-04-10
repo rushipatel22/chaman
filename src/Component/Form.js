76217e1fed006d1568bda59e297251b9015510ca
Rushi <rushi@techtic.agency>
2023-04-10 11:29:24 +0530

    changes

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -1,19 +1,37 @@
 import React from 'react';
 function Form() {
     return(
-        <div className="form-body">
-        <div className="firstname">
-            <label>First Name: &nbsp;</label>
-            <input type="text" placeholder="First Name"/>
+    <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-7">
+            <div class="card mt-3">
+                <div class="card-body">
+                        <div class="form-group">
+                            <label for="Name">Name</label>
+                            <input type="text" class="form-control"  placeholder="Enter name"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="Name">Email</label>
+                            <input type="text" class="form-control" placeholder="Enter email"/>
+                        </div>
+                        <label for="password">Password</label>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <input type="text" class="form-control " placeholder="Enter Password"/>
+                            </div>
+                        </div>
+                        <label for="confirm_password">Confirm Password</label>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <input type="text" class="form-control input-lg" placeholder="Enter Confirm Password"/>
+                            </div> 
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
         </div>
-        <div className="lastname">
-            <label>Last Name: &nbsp; </label>
-            <input  type="text" placeholder="LastName"/>
-        </div> 
-        <div>
-              <button type="submit">Register</button>
-        </div>     
-    </div>     
+    </div>
+</div>   
     )       
 }
 export default Form;
