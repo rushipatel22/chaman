5c84dfd3476fd0b6724eb859732fff1ddac385a3
Rushi <rushi@techtic.agency>
2023-04-27 17:00:48 +0530

    fix issue

--- src/Component/Todo.js
+++ src/Component/Todo.js
@@ -7,60 +7,72 @@ import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import EditIcon from '@mui/icons-material/Edit';
 import { useState } from 'react';
 const Todo = () => {
-    const [form, setForm] = useState({});
-    const [data, setData] = useState([]);
-    const [toggleBtn, setToggleBtn] = useState(true);
-    const [iseditItem, setIsEditItem] = useState(null);
-    console.log(data)
-    const addData = () => {
-      if (!form.project_name || !form.task_name) { // check if project_name and task_name fields are empty
-        alert('please fill data')
-      } else if (iseditItem !== null) { // check if editing an existing item
-        setData(
-          data.map((element) => {
-            if (element.id === iseditItem) {
-              return { ...element, project_name: form.project_name, task_name: form.task_name } // update project_name and task_name fields of edited item
-            }
-            return element;
-          })
-        )
-        setForm({});
-        setIsEditItem(null);
-        setToggleBtn(true);
-      } else { // adding a new item
-        const inputData = { id: new Date().getTime().toString(), project_name: form.project_name, task_name: form.task_name }
-        setData([...data, inputData]);
-        setForm({});
-      }
+  const [form, setForm] = useState({});
+  const [data, setData] = useState([]);
+  const [toggleBtn, setToggleBtn] = useState(true);
+  const [iseditItem, setIsEditItem] = useState(null);
+  console.log(form);
+  const addData = () => {
+    if (!form.project_name || !form.task_name) { // check if project_name and task_name fields are empty
+      alert('please fill data')
+    } else if (iseditItem !== null) { // check if editing an existing item
+      setData(
+        data.map((element) => {
+          if (element.id === iseditItem) {
+            return { ...element, project_name: form.project_name, task_name: form.task_name } // update project_name and task_name fields of edited item
+          }
+          return element;
+        })
+      )
+      setForm({ project_name: '', task_name: '' });
+      setIsEditItem(null);
+      setToggleBtn(true);
+    } else { // adding a new item
+      const inputData = { id: new Date().getTime().toString(), project_name: form.project_name, task_name: form.task_name }
+      setData([...data, inputData]);
+      setForm({ project_name: '', task_name: '' });
     }
-    const removeItem = (index) => {
-      const deleteditems = data.filter((element) => {
-        return index !== element.id;
-      });
-      setData(deleteditems);
-    }
-    const editItem = (id) => {
-      let newItem = data.find((element) => {
-          return element.id === id;
-      })
-      console.log(newItem);
-      setToggleBtn(false);
-      setForm(newItem);
-      setIsEditItem(id);
-    }
+  }
+  const removeItem = (index) => {
+    const deleteditems = data.filter((element) => {
+      return index !== element.id;
+    });
+    setData(deleteditems);
+  }
+  const editItem = (id) => {
+    let newItem = data.find((element) => {
+      return element.id === id;
+    })
+    console.log(newItem);
+    setToggleBtn(false);
+    setForm(newItem);
+    setIsEditItem(id);
+  }
   return (
     <div className="todo">
       <div className="form">
         <Stack spacing={2} direction="row">
-          <TextField value={form.project_name} onChange={(e) => setForm({...form,project_name: e.target.value})} id="outlined-basic" label="Project Name" variant="outlined" />
-          <TextField value={form.task_name} onChange={(e) => setForm({...form,task_name: e.target.value})} id="outlined-basic" label="Task Name" variant="outlined" />
-              <Button onClick={addData} variant="contained" color="success">
-                {toggleBtn ? <AddIcon /> : <EditIcon />}
-              </Button>
+          <TextField
+            value={form.project_name || ''}
+            onChange={(e) => setForm({ ...form, project_name: e.target.value })}
+            id="outlined-basic"
+            label="Project Name"
+            variant="outlined"
+          />
+          <TextField
+            value={form.task_name || ''}
+            onChange={(e) => setForm({ ...form, task_name: e.target.value })}
+            id="outlined-basic"
+            label="Task Name"
+            variant="outlined"
+          />
+          <Button onClick={addData} variant="contained" color="success">
+            {toggleBtn ? <AddIcon /> : <EditIcon />}
+          </Button>
         </Stack>
       </div>
       <div className="data">
-        <div className="data_view"> 
+        <div className="data_view">
           <h4>Project Name</h4>
           <h4>Task Name</h4>
           <h4>Remove</h4>
@@ -73,18 +85,18 @@ const Todo = () => {
                 <h4>{element.project_name}</h4>
                 <h4>{element.task_name}</h4>
                 <h4>
-                <Stack>
+                  <Stack>
                     <Button onClick={() => removeItem(element.id)} variant="contained" color="error">
                       <DeleteForeverIcon />
                     </Button>
-                </Stack>
+                  </Stack>
                 </h4>
                 <h4>
-                <Stack>
+                  <Stack>
                     <Button onClick={() => editItem(element.id)} variant="contained" color="success">
                       <EditIcon />
-                  </Button>
-                </Stack>
+                    </Button>
+                  </Stack>
                 </h4>
               </div>
             )
@@ -94,5 +106,4 @@ const Todo = () => {
     </div>
   )
 }
-
 export default Todo
